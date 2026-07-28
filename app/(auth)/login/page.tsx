93447bc5065daf3d15af5b85f0503b43fea4122e
mahalthy <mahalthykhaleel@gmail.com>
2026-07-28 17:29:23 +0530

    updated link help doc  component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -9,12 +9,10 @@ const LoginPage = () => {
     <div className="min-h-screen bg-gray-100 py-10">
       <div className="w-full max-w-7xl mx-auto bg-white rounded-xl shadow-lg border border-gray-200 p-10">
         {/* <LoginForm /> */}
-        <a
-          href="/login"
-          className="md-2-block bg-blue-600 hover:bg-blue-700 text-white font-semibold px-5 py-2.5 rounded-md shadow-md transition duration-200"
-        >
+        <Link href="/login" className="hover:text-blue-600 mr-5">
           Log in
-        </a>
+        </Link>
+
         <NextDocForm />
       </div>
     </div>
