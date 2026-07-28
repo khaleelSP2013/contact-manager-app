471127dac928993606854527bbe2a1aa56fff0ff
mahalthy <mahalthykhaleel@gmail.com>
2026-07-28 17:22:48 +0530

    updated help doc  component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -8,9 +8,8 @@ const LoginPage = () => {
   return (
     <div className="min-h-screen bg-gray-100 py-10">
       <div className="w-full max-w-7xl mx-auto bg-white rounded-xl shadow-lg border border-gray-200 p-10">
-        <h1 className="text-2xl font-bold md-6"> Next js Docmentation</h1>
         {/* <LoginForm /> */}
-        <Link href="/login"></Link>
+        <Link href="/login">Click here to Login </Link>
         <NextDocForm />
       </div>
     </div>
